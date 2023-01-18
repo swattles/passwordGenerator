b9126b52f6243837e0fb47731196bdfa5e62eb1d
Sarah Wattles <swattles@swattles.onmicrosoft.com>
2023-01-18 18:45:14 -0500

    add files

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,27 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtnEl = document.querySelector("#generate-btn");
+
+// Returns random characters, including alphanumeric and special character values
+function getCharacters() {
+  return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
+}
+
+// Generates the password as a string of characters of specified length (num)
+function createPassword(num) {
+  var password = '';
+  for (var i =0; i < num; i++) {
+    password += getCharacters();
+  }
+  return password;
+}
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword () {
+    var newPassword = createPassword(15); 
+    var passwordDisplayEl = document.querySelector("#password-display");
 
-  passwordText.value = password;
-
+    passwordDisplayEl.value = newPassword;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtnEl.addEventListener("click", writePassword);
